825e9b549c0e600acfcfe151ba72c3adb9a3a3fe
chinna nani <chinnaaaa225@gmail.com>
2022-08-06 15:57:59 +0530

    shell scripting print lines colours lines between tab command scripting and syntex.

--- 02-print.sh
+++ 02-print.sh
@@ -3,3 +3,24 @@ echo hallo
 # some times we need print muti lines
 
 echo -e "Hello\nWorld"
+# syntx : -e is needed to print multi lines/ infact to enable  \n  we need -e
+#         quotes are mandatry for using any \ esc seq ,we used new line esc seq
+#        \n is print to new line
+
+# Tabbed line
+
+echo -e "one\t\ttwo"
+
+# color printing
+
+echo -e "\e[31mhello\e[0m"
+echo -e "\e[31mhello\e[0m"
+echo -e "\e[31mhello\e[0m"
+echo -e "\e[31mhello\e[0m"
+echo -e "\e[31mhello\e[0m"
+echo -e "\e[31mhello\e[0m"
+
+#syntex :  -e is enable \e
+#           \e[31m 31 is colour code
+#           \e[0m  0 is reset the color
+##note : in shell when we enable any colour and it is our reponsibilty to disable it.
