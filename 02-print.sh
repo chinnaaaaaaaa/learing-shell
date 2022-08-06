1c652537e0ca5a0fcd9ceb97c9d567266b1b35a3
chinna nani <chinnaaaa225@gmail.com>
2022-08-06 16:04:02 +0530

    colours and its code numbers

--- 02-print.sh
+++ 02-print.sh
@@ -14,13 +14,21 @@ echo -e "one\t\ttwo"
 # color printing
 
 echo -e "\e[31mhello\e[0m"
-echo -e "\e[31mhello\e[0m"
-echo -e "\e[31mhello\e[0m"
-echo -e "\e[31mhello\e[0m"
-echo -e "\e[31mhello\e[0m"
-echo -e "\e[31mhello\e[0m"
+echo -e "\e[32mhello\e[0m"
+echo -e "\e[33mhello\e[0m"
+echo -e "\e[34mhello\e[0m"
+echo -e "\e[35mhello\e[0m"
+echo -e "\e[36mhello\e[0m"
 
 #syntex :  -e is enable \e
 #           \e[31m 31 is colour code
 #           \e[0m  0 is reset the color
 ##note : in shell when we enable any colour and it is our reponsibilty to disable it.
+
+# colour  # colour code
+#  red           31
+#  green         32
+#  yellow        33
+#  blue          34
+#  magenta       35
+#  cyan          36
