8c8039730f4522ac42e46c0488883ed7bcc3471a
chinna nani <chinnaaaa225@gmail.com>
2022-08-08 17:31:22 +0530

    function vic-vars command

--- 05-function.sh
+++ 05-function.sh
@@ -3,8 +3,12 @@
 ## Declare a function
 sample() {
   echo sample function
+  echo a from main program = $a
+  b=20
   }
 
 ## Access a function , simply function name
+a=10
 sample
+echo b from function =$b
 
